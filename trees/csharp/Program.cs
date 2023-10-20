c3973383fe8667b9c76e6bb43f5f6f163b81f936
James Dean <jamesdean268@gmail.com>
2023-10-20 16:13:44 +1100

    Added traversal methods for C#

--- trees/csharp/Program.cs
+++ trees/csharp/Program.cs
@@ -12,14 +12,137 @@ class Program
             this.right = right;
         }
     }
-    
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        {
+            // ------------------------------------ Traversal Function Test -----------------------
+            TreeNode root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
+
+            // ----- PreOrder -----
+            IList<int> preOrder = PreorderTraversal(root);
+            Console.Write("[");
+            foreach (int i in preOrder) {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine("]");
+
+            // ----- InOrder -----
+            IList<int> inOrder = InorderTraversal(root);
+            Console.Write("[");
+            foreach (int i in inOrder) {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine("]");
+
+            // ----- PostOrder -----
+            IList<int> postOrder = PostorderTraversal(root);
+            Console.Write("[");
+            foreach (int i in postOrder) {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine("]");
+        }
     }
 
+    // ------------------------------------------------------------------------------------
+    // Leetcode Question: Binary Tree Preorder Traversal
+    // 
+    // Given the root of a binary tree, return the preorder traversal of its nodes' values.
+    // Root, Left, Right
+    //
+    // Input: root = [1,null,2,3]
+    // Output: [1,2,3]
+    //
+    // https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/928/
+    // ------------------------------------------------------------------------------------
+    static IList<int> PreorderTraversal(TreeNode root) {
+        List<int> list = new List<int>();
+        if (root == null) return list;
+        // Add Root
+        list.Add(root.val);
+        // Recursively traverse left tree
+        if (root.left != null) {
+            IList<int> leftList = PreorderTraversal(root.left);
+            foreach (int i in leftList) {
+                list.Add(i);
+            }
+        }
+        // Recursively traverse right tree
+        if (root.right != null) {
+            IList<int> rightList = PreorderTraversal(root.right);
+            foreach (int i in rightList) {
+                list.Add(i);
+            }
+        }
+        return (IList<int>) list;
+    }
 
-
- 
+    // ------------------------------------------------------------------------------------
+    // Leetcode Question: Binary Tree Inorder Traversal
+    // 
+    // Given the root of a binary tree, return the inorder traversal of its nodes' values.
+    // Left, Root, Right
+    //
+    // Input: root = [1,null,2,3]
+    // Output: [1,3,2]
+    //
+    // https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/
+    // ------------------------------------------------------------------------------------
+    static IList<int> InorderTraversal(TreeNode root) {
+        List<int> list = new List<int>();
+        if (root == null) return list;
+        // Recursively traverse left tree
+        if (root.left != null) {
+            IList<int> leftList = InorderTraversal(root.left);
+            foreach (int i in leftList) {
+                list.Add(i);
+            }
+        }
+        // Add Root
+        list.Add(root.val);
+        // Recursively traverse right tree
+        if (root.right != null) {
+            IList<int> rightList = InorderTraversal(root.right);
+            foreach (int i in rightList) {
+                list.Add(i);
+            }
+        }
+        return (IList<int>) list;
+    }
+     // ------------------------------------------------------------------------------------
+    // Leetcode Question: Binary Tree Postorder Traversal
+    // 
+    // Given the root of a binary tree, return the postorder traversal of its nodes' values.
+    // Left, Right, Root
+    //
+    // Input: root = [1,null,2,3]
+    // Output: [3,2,1]
+    //
+    // https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/930/
+    // ------------------------------------------------------------------------------------
+    static IList<int> PostorderTraversal(TreeNode root) {
+        List<int> list = new List<int>();
+        if (root == null) return list;
+        // Recursively traverse left tree
+        if (root.left != null) {
+            IList<int> leftList = PostorderTraversal(root.left);
+            foreach (int i in leftList) {
+                list.Add(i);
+            }
+        }
+        // Recursively traverse right tree
+        if (root.right != null) {
+            IList<int> rightList = PostorderTraversal(root.right);
+            foreach (int i in rightList) {
+                list.Add(i);
+            }
+        }
+        // Add Root
+        list.Add(root.val);
+        return (IList<int>) list;
+    }
 
 }
